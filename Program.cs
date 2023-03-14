929892cbf33f3340f4ebb29f18b971e4384a6e65
ViniWixak <viniciuswixak@gmail.com>
2023-03-14 17:30:57 -0300

    Implementação do Migrations e Injeção de dependência

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
+using MinimalApi.Data;
+using System;
+
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<DataContext>();
 var app = builder.Build();
 
 app.MapGet("/", () => Results.Ok(new Todo(Guid.NewGuid(), "Ir a academia", false)));
